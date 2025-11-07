559cfdefb233bdde1006bb3b70e5a061ebea2d9a
akbar <akbarsafari00@gmail.com>
2025-11-07 18:15:13 +0330

    111

--- next.config.ts
+++ next.config.ts
@@ -140,7 +140,6 @@ const pwaConfig = withPWA({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
